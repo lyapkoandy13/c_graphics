02bc5e314ee5e3d0a588167c8c582ee93a942278
Andrii Liapko <lyapkoandy13@gmail.com>
2024-02-01 15:39:46 +0200

    fix: CMake config

--- c_graphics/src/files.c
+++ c_graphics/src/files.c
@@ -1,45 +1,60 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-char *readFile(const char *path)
-{
-    FILE *file = NULL;
-    file = fopen(path, "r");
+char *concat(const char *s1, const char *s2) {
+  char *result =
+      malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
 
-    if (file == NULL)
-    {
-        perror("Could not open a file");
-    }
-
-    size_t resultLen = 0, resultSize = 0;
-    char *result = NULL, *tmp = NULL;
-
-    char line[1024];
-    while (fgets(line, sizeof(line), file))
-    {
-        size_t lineLen = strlen(line);
-
-        // allocate memory if new line doesnt fit
-        // -1 because we remove 1 of the \0
-        if ((lineLen + resultLen - 1) > resultSize)
-        {
-            resultSize += sizeof(line);
-            tmp = realloc(result, resultSize);
-
-            if (tmp == NULL)
-            {
-                perror("Failed to realloc for final string while reading a file");
-                break;
-            }
-
-            result = tmp;
-        }
-
-        strcat(result, line);
-    }
-
-    fclose(file);
-
+  if (result == NULL) {
+    perror("Could not malloc");
     return result;
+  }
+
+  strcpy(result, s1);
+  strcat(result, s2);
+  return result;
+}
+
+char *readFile(const char *path) {
+  FILE *file = NULL;
+  file = fopen(path, "r");
+
+  if (file == NULL) {
+    char *errMsg = concat("Could not open a file ", path);
+    if (errMsg != NULL) {
+      perror(errMsg);
+      free(errMsg);
+    }
+    perror("Could not open a file");
+    exit(1);
+  }
+
+  size_t resultLen = 0, resultSize = 0;
+  char *result = NULL, *tmp = NULL;
+
+  char line[1024];
+  while (fgets(line, sizeof(line), file)) {
+    size_t lineLen = strlen(line);
+
+    // allocate memory if new line doesnt fit
+    // -1 because we remove 1 of the \0
+    if ((lineLen + resultLen - 1) > resultSize) {
+      resultSize += sizeof(line);
+      tmp = realloc(result, resultSize);
+
+      if (tmp == NULL) {
+        perror("Failed to realloc for final string while reading a file");
+        break;
+      }
+
+      result = tmp;
+    }
+
+    strcat(result, line);
+  }
+
+  fclose(file);
+
+  return result;
 }
--- c_graphics/src/main.c
+++ c_graphics/src/main.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "files.h"
+#ifdef __APPLE__
+/* Defined before OpenGL and GLUT includes to avoid deprecation messages */
+#define GL_SILENCE_DEPRECATION
+#endif
+// clang-format off
+// #define GLFW_INCLUDE_NONE // https://stackoverflow.com/questions/44345811/glad-h-giving-error-that-opengl-header-is-included
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+// clang-format on
+#include <limits.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include "files.h"
+#include <unistd.h>
 
-#define VERT_SHADER_SRC "src/shaders/default.vert"
-#define FRAG_SHADER_SRC "src/shaders/default.frag"
+#define VERT_SHADER_SRC "../../c_graphics/shaders/default.vert"
+#define FRAG_SHADER_SRC "../../c_graphics/shaders/default.frag"
 
 #define TRUE 1
 #define FALSE 0
@@ -16,180 +25,169 @@ const int WINDOW_HEIGHT = 600;
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
-/// @brief
+/// @brief sets up vert- and frag- shaders and creates a program
 /// @return shader program id
-unsigned int createShader(const char *vertSrcPath, const char *fragSrcPath)
-{
-    const char *vertSrc = readFile(vertSrcPath);
-    unsigned int vshader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vshader, 1, &vertSrc, NULL);
-    glCompileShader(vshader);
+unsigned int createShader(const char *vertSrcPath, const char *fragSrcPath) {
+  const char *vertSrc = readFile(vertSrcPath);
 
-    int success;
-    char infoLog[1024];
-    glGetShaderiv(vshader, GL_COMPILE_STATUS, &success);
-    if (success != GL_TRUE)
-    {
-        glGetShaderInfoLog(vshader, 1024, NULL, infoLog);
-        printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
-        puts(infoLog);
-    }
+  unsigned int vshader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vshader, 1, &vertSrc, NULL);
+  glCompileShader(vshader);
 
-    const char *fragSrc = readFile(fragSrcPath);
-    unsigned int fshader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fshader, 1, &fragSrc, NULL);
-    glCompileShader(fshader);
+  int success;
+  char infoLog[1024];
+  glGetShaderiv(vshader, GL_COMPILE_STATUS, &success);
+  if (success != GL_TRUE) {
+    glGetShaderInfoLog(vshader, 1024, NULL, infoLog);
+    printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
+    puts(infoLog);
+  }
 
-    glGetShaderiv(fshader, GL_COMPILE_STATUS, &success);
-    if (success != GL_TRUE)
-    {
-        glGetShaderInfoLog(fshader, 1024, NULL, infoLog);
-        printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
-        puts(infoLog);
-    }
+  const char *fragSrc = readFile(fragSrcPath);
 
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vshader);
-    glAttachShader(shaderProgram, fshader);
-    glLinkProgram(shaderProgram);
+  unsigned int fshader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fshader, 1, &fragSrc, NULL);
+  glCompileShader(fshader);
 
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        printf("ERROR::SHADER::LINKING_FAILED\n");
-        puts(infoLog);
-    }
+  glGetShaderiv(fshader, GL_COMPILE_STATUS, &success);
+  if (success != GL_TRUE) {
+    glGetShaderInfoLog(fshader, 1024, NULL, infoLog);
+    printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n");
+    puts(infoLog);
+  }
 
-    puts(" ");
-    puts(vertSrc);
-    puts(fragSrc);
+  unsigned int shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vshader);
+  glAttachShader(shaderProgram, fshader);
+  glLinkProgram(shaderProgram);
 
-    // TODO WTF
-    // free(vertSrc);
-    // free(fragSrc);
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    printf("ERROR::SHADER::LINKING_FAILED\n");
+    puts(infoLog);
+  }
 
-    // TODO?: delete shader sources
-    glDeleteShader(vshader);
-    glDeleteShader(fshader);
+  // TODO why
+  // free(vertSrc);
+  // free(fragSrc);
 
-    return shaderProgram;
+  // TODO?: delete shader sources
+  glDeleteShader(vshader);
+  glDeleteShader(fshader);
+
+  return shaderProgram;
+}
+
+void printCurrDir(void) {
+  char cwd[PATH_MAX];
+  if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    printf("Current working dir: %s\n", cwd);
+  } else {
+    perror("getcwd() error");
+  }
 }
 
 // ENTRY POINT
 // -------------
-int main(void)
-{
-    // glfw: initialize and configure
-    // ------------------------------
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+int main(void) {
+  printCurrDir();
+
+  if (!glfwInit()) {
+    perror("GL initialization failed");
+    return -1;
+  };
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    // glfw window creation
-    // --------------------
-    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Bad Engine v1", NULL, NULL);
-    if (window == NULL)
-    {
-        puts("Failed to create GLFW window");
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        puts("Failed to initialize GLAD");
-        return -1;
-    }
-
-    unsigned int shader = createShader(VERT_SHADER_SRC, FRAG_SHADER_SRC);
-
-    // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
-    float vertices[] = {
-        // positions         // colors
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
-        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // top
-    };
-
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    // glBindVertexArray(0);
-
-    // render loop
-    // -----------
-    while (!glfwWindowShouldClose(window))
-    {
-        // input
-        // -----
-        processInput(window);
-
-        // render
-        // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // render the triangle
-        glUseProgram(shader);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
+  GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+                                        "Bad Engine v1", NULL, NULL);
+  if (window == NULL) {
+    puts("Failed to create GLFW window");
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // glad: load all OpenGL function pointers
+  // ---------------------------------------
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    puts("Failed to initialize GLAD");
+    return -1;
+  }
+
+  unsigned int shader = createShader(VERT_SHADER_SRC, FRAG_SHADER_SRC);
+
+  float vertices[] = {
+      // positions         // colors
+      0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+      0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f  // top
+  };
+
+  unsigned int VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shader);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
+    // etc.)
+    // -------------------------------------------------------------------------------
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // optional: de-allocate all resources once they've outlived their purpose:
+  // ------------------------------------------------------------------------
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+
+  // glfw: terminate, clearing all previously allocated GLFW resources.
+  // ------------------------------------------------------------------
+  glfwTerminate();
+  return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// process all input: query GLFW whether relevant keys are pressed/released this
+// frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, TRUE);
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, TRUE);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// glfw: whenever the window size changed (by OS or user resize) this callback
+// function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(__attribute__((unused)) GLFWwindow *window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and
-    // height will be significantly larger than specified on retina displays.
-    glViewport(0, 0, width, height);
+void framebuffer_size_callback(__attribute__((unused)) GLFWwindow *window,
+                               int width, int height) {
+  // make sure the viewport matches the new window dimensions; note that width
+  // and height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
 }
